6cf4405e2560ae17e3132dd3c2ecafbf0081de22
Elios Dauti <edauti@rocketmail.com>
2022-05-24 09:20:23 +0100

    removed commented code for testing

--- Exercises/FizzBuzz.py
+++ Exercises/FizzBuzz.py
@@ -27,16 +27,3 @@ while choice != "2":
     else:
         print("Incorrect input, please try again.")
         print()
-
-
-
-
-# for count in range(1, 51):
-#     if count % 3 == 0 and count % 5 == 0:
-#         print("FizzBuzz")
-#     elif count % 3 == 0:
-#         print("Fizz")
-#     elif count % 5 == 0:
-#         print("Buzz")
-#     else:
-#         print(count)
